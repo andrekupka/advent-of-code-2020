d89f5ae79eca7d21ed45f032694a1e6eedde8b09
Andre Kupka <me-github@andrekupka.de>
2020-12-07 22:23:20 +0100

    improve parser

--- src/main/kotlin/de/andrekupka/adventofcode/day7/Day7.kt
+++ src/main/kotlin/de/andrekupka/adventofcode/day7/Day7.kt
@@ -1,4 +1,13 @@
 package de.andrekupka.adventofcode.day7
 
-class Day7 {
+import com.github.h0tk3y.betterParse.grammar.parseToEnd
+import de.andrekupka.adventofcode.utils.readLinesNotBlank
+
+fun main(args: Array<String>) {
+    val lines = readLinesNotBlank(args[0])
+
+    lines.forEach {
+        val rule = LuggageParser.parseToEnd(it)
+        println(rule)
+    }
 }
--- src/main/kotlin/de/andrekupka/adventofcode/day7/LuggageRuleParser.kt
+++ src/main/kotlin/de/andrekupka/adventofcode/day7/LuggageRuleParser.kt
@@ -1,2 +1,53 @@
 package de.andrekupka.adventofcode.day7
 
+import com.github.h0tk3y.betterParse.combinators.*
+import com.github.h0tk3y.betterParse.grammar.Grammar
+import com.github.h0tk3y.betterParse.lexer.literalToken
+import com.github.h0tk3y.betterParse.lexer.regexToken
+
+data class BagCount(
+    val count: Int,
+    val color: String
+)
+
+data class LuggageRule(
+    val color: String,
+    val bags: List<BagCount>
+)
+
+val LuggageParser = object : Grammar<LuggageRule>() {
+    val contain by literalToken("contain")
+    val no by literalToken("no")
+    val other by literalToken("other")
+    val dot by literalToken(".")
+    val comma by literalToken(",")
+
+    val bag by regexToken("bags|bag")
+    val number by regexToken("[1-9]+[0-9]*")
+    val identifier by regexToken("[a-z]+")
+
+    val whitespace by regexToken("\\s+", ignore = true)
+
+    val numberParser by number use { text.toInt() }
+
+    val colorParser = (1..2 times identifier) use {
+        joinToString(" ") { it.text }
+    }
+
+    val bagCountParser by numberParser * colorParser * -bag use {
+        BagCount(t1, t2)
+    }
+
+    val multipleBagCountParser by separatedTerms(bagCountParser, comma)
+    val noBagCountParser by -no * -other * bag use { emptyList<BagCount>() }
+
+    val bagCountAlternativeParser by multipleBagCountParser or noBagCountParser
+
+    val startParser by colorParser * -bag * -contain
+
+    val ruleParser = startParser * bagCountAlternativeParser * -dot use {
+        LuggageRule(t1, t2)
+    }
+
+    override val rootParser get() = ruleParser
+}
