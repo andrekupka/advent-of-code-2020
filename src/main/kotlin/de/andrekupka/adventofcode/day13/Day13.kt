3df8b3d202644221ca22c2639b2db7ecbb42a0b1
Andre Kupka <me-github@andrekupka.de>
2020-12-13 14:29:19 +0100

    day 13 part 1

--- src/main/kotlin/de/andrekupka/adventofcode/day13/Day13.kt
+++ src/main/kotlin/de/andrekupka/adventofcode/day13/Day13.kt
@@ -4,6 +4,7 @@ import de.andrekupka.adventofcode.utils.readLinesNotBlank
 
 fun main(args: Array<String>) {
     val lines = readLinesNotBlank(args[0])
+    //val lines = listOf("939", "7,13,x,x,59,x,31,19")
 
     if (lines.size != 2) {
         println("Expected exactly 2 lines")
@@ -20,6 +21,7 @@ fun main(args: Array<String>) {
     val (optimalBusId, waitingTime) = busIdsWithDepartureDifference.minByOrNull { it.second }!!
 
     println("Optimal bus id is $optimalBusId")
+    println("Waiting time is $waitingTime")
 
     val busIdMultipliedByTime = optimalBusId * waitingTime
     println("Waiting time multiplied by bus id is $busIdMultipliedByTime")
