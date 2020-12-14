8394477e95b03112d1b53b89d3e0dcea6256fc1d
Andre Kupka <me-github@andrekupka.de>
2020-12-14 08:39:59 +0100

    day 13 part 2

--- src/main/kotlin/de/andrekupka/adventofcode/day13/Day13.kt
+++ src/main/kotlin/de/andrekupka/adventofcode/day13/Day13.kt
@@ -2,18 +2,8 @@ package de.andrekupka.adventofcode.day13
 
 import de.andrekupka.adventofcode.utils.readLinesNotBlank
 
-fun main(args: Array<String>) {
-    val lines = readLinesNotBlank(args[0])
-    //val lines = listOf("939", "7,13,x,x,59,x,31,19")
 
-    if (lines.size != 2) {
-        println("Expected exactly 2 lines")
-        return
-    }
-
-    val departureTime = lines[0].toInt()
-    val busIds = lines[1].split(",").mapNotNull { it.toIntOrNull() }
-
+fun solvePart1(departureTime: Long, busIds: List<Long>) {
     println("Departure time: $departureTime")
     println("Bus ids: $busIds")
 
@@ -25,4 +15,45 @@ fun main(args: Array<String>) {
 
     val busIdMultipliedByTime = optimalBusId * waitingTime
     println("Waiting time multiplied by bus id is $busIdMultipliedByTime")
+}
+
+
+fun solvePart2(busIdsWithOffset: List<Pair<Long, Long>>) {
+    println("Bus ids with offsets are $busIdsWithOffset")
+
+    var timestamp = 0L
+    var step = 1L
+
+    for ((busId, offset) in busIdsWithOffset) {
+        val steppedTimestamps = generateSequence(timestamp) { it + step }
+        for (nextTimestamp in steppedTimestamps) {
+            if ((nextTimestamp + offset) % busId == 0L) {
+                timestamp = nextTimestamp
+                step *= busId
+                break
+            }
+        }
+    }
+
+    println("First timestamp that fulfills offset constraints is $timestamp")
+}
+
+fun main(args: Array<String>) {
+    val lines = readLinesNotBlank(args[0])
+
+    if (lines.size != 2) {
+        println("Expected exactly 2 lines")
+        return
+    }
+
+    val departureTime = lines[0].toLong()
+    val busIds = lines[1].split(",").mapNotNull { it.toLongOrNull() }
+
+    solvePart1(departureTime, busIds)
+
+    val busIdsWithOffset = lines[1].split(",")
+        .map { it.toLongOrNull() }
+        .mapIndexedNotNull { index, id -> id?.let { id to index.toLong() } }
+
+    solvePart2(busIdsWithOffset)
 }
